ad89ef4b512840ff1dc2e49554181e904de73e80
Eric Meyer <emeyer@8thlight.com>
2010-12-20 08:51:06 -0600

    writing methods for board

--- spec/board_spec.clj
+++ spec/board_spec.clj
@@ -1,14 +1,22 @@
-(ns basics-spec
+(ns board-spec
   (:use [speclj.core]
         [ttt_clojure.board])
   (:require [ttt_clojure.board]))
 
 (describe "Board"
-
   (it "is empty"
-    (should (board-empty? [[- - -] [- - - ] [- - -]])))
-
-  (it "is not false"
-    (should-not false)))
+    (should (board-empty? [["-" "-" "-"] ["-" "-" "-"] ["-" "-" "-"]])))
+	(it "is not empty if X is in the first row"
+		(should-not (board-empty? [["X" "-" "-"] ["-" "-" "-"] ["-" "-" "-"]])))
+	(it "is not empty if O is in the first row"
+		(should-not (board-empty? [["O" "-" "-"] ["-" "-" "-"] ["-" "-" "-"]])))
+	(it "is not empty if X is in the second row"
+		(should-not (board-empty? [["-" "-" "-"] ["-" "-" "X"] ["-" "-" "-"]])))
+		
+	(it "is a valid move if is isn't taken"
+		(should (valid-move? [["-" "-" "-"] ["-" "-" "-"] ["-" "-" "-"]] 0 0)))
+	(it "is not a valid move if is is taken"
+		(should-not (valid-move? [["-" "X" "-"] ["-" "-" "-"] ["-" "-" "-"]] 0 1)))
+)
 
 (run-specs)
--- src/ttt_clojure/board.clj
+++ src/ttt_clojure/board.clj
@@ -1,4 +1,10 @@
 (ns ttt_clojure.board)
 
-(defn board-empty? [x]
-  true)
+(defn board-empty? [board]
+	(every? #(= "-" %) (flatten board)))
+
+(defn- spot-empty? [board row col]
+	(= "-" ((board row) col)))
+	
+(defn valid-move? [board row col]
+	(spot-empty? board row col))
