bde7efa974b2aca6f78fd3048d0baf70cf8bfe4b
Eric Meyer <emeyer@8thlight.com>
2010-12-21 17:40:26 -0600

    check for winner complete

--- spec/board_spec.clj
+++ spec/board_spec.clj
@@ -17,6 +17,32 @@
 		(should (valid-move? [["-" "-" "-"] ["-" "-" "-"] ["-" "-" "-"]] 0 0)))
 	(it "is not a valid move if is is taken"
 		(should-not (valid-move? [["-" "X" "-"] ["-" "-" "-"] ["-" "-" "-"]] 0 1)))
+	
+	(it "gets the first column"
+		(should= ["1" "4" "7"] (column 0 [["1" "2" "3"] ["4" "5" "6"] ["7" "8" "9"]])))
+	(it "gets the second column"
+		(should= ["2" "5" "8"] (column 1 [["1" "2" "3"] ["4" "5" "6"] ["7" "8" "9"]])))
+		
+	(it "has no winner for an empty board"
+		(should-not (winner-exists? [["-" "-" "-"] ["-" "-" "-"] ["-" "-" "-"]])))
+	(it "has a winner if X completes the first row"
+		(should (winner-exists? [["X" "X" "X"] ["-" "-" "-"] ["-" "-" "-"]])))
+	(it "has a winner if O completes the first row"
+		(should (winner-exists? [["O" "O" "O"] ["-" "-" "-"] ["-" "-" "-"]])))
+	(it "has a winner if X completes the second row"
+		(should (winner-exists? [["O" "X" "O"] ["X" "X" "X"] ["-" "-" "-"]])))
+	(it "has a winner if X completes the first column"
+		(should (winner-exists? [["X" "-" "-"] ["X" "-" "-"] ["X" "-" "-"]])))
+	(it "has a winner if O completes the first column"
+		(should (winner-exists? [["O" "X" "O"] ["O" "-" "-"] ["O" "-" "-"]])))
+	(it "has a winner if X completes the second column"
+		(should (winner-exists? [["O" "X" "O"] ["-" "X" "O"] ["-" "X" "-"]])))
+	(it "has a winner if X completes the third column"
+		(should (winner-exists? [["O" "X" "X"] ["-" "O" "X"] ["-" "X" "X"]])))
+	(it "has a winner if X completes one diagonal"
+		(should (winner-exists? [["X" "X" "-"] ["-" "X" "X"] ["O" "-" "X"]])))
+	(it "has a winner if X completes the other diagonal"
+		(should (winner-exists? [["O" "X" "O"] ["-" "O" "X"] ["O" "-" "X"]])))
 )
 
 (run-specs)
--- src/ttt_clojure/board.clj
+++ src/ttt_clojure/board.clj
@@ -8,3 +8,31 @@
 	
 (defn valid-move? [board row col]
 	(spot-empty? board row col))
+
+(defn- player-wins-in-group? [player group]
+	(every? #(= player %) group))
+	
+(defn- winner-in-group? [group]
+	(or (player-wins-in-group? "X" group) (player-wins-in-group? "O" group)))
+	
+(defn column [n board]
+	(map #(% n) board))
+
+(defn- columns [board]
+	(map #(column % board) (range (count board))))
+
+(defn- rows [board]
+	board)
+	
+(defn- winner-in-rows? [board]
+	(some #(winner-in-group? %) (rows board)))
+
+(defn- winner-in-columns? [board]
+	(some #(winner-in-group? %) (columns board)))
+
+(defn- winner-in-diagonal? [board]
+	(or (winner-in-group? [((board 0) 0) ((board 1) 1) ((board 2) 2)])
+			(winner-in-group? [((board 2) 0) ((board 1) 1) ((board 0) 2)])))
+	
+(defn winner-exists? [board]
+	(or (winner-in-rows? board) (winner-in-columns? board) (winner-in-diagonal? board)))
